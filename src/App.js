3626a5ebee50800be422b3cedc3fa05625ac2add
mishkat01 <mishkatislam007@gmail.com>
2025-08-20 22:21:18 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useFormState } from 'react-dom';
 
 function App() {
 
   const  [noteTitle,setNoteTitle]=useState('');
+  const  [note,setNote]    =useState([]);
+  const  [editMode,setEditMode] =useState(false);
+  const  [editablenote,setEditableNote] = useState(null);
+
+
+  
+
  
   return (
     <div className="App">
