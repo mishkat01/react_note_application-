ffa6f7df0e7ed0b808607f8e1cf1d494fa1894ad
mishkat01 <mishkatislam007@gmail.com>
2025-08-20 22:10:51 +0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const  [noteTitle,setNoteTitle]=useState('');
+ 
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
